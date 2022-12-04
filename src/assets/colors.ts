9c828e30a4801a1759b034e1e2b1619101307de7
Luciano Weslen <luciano.weslen@zrp.com.br>
2022-12-04 14:03:12 -0300

    feat: updated colors assets

--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -3,3 +3,13 @@ export const LUOP_COLORS = {
   secondary: "#1E2329",
   white: "#FFFFFF",
 };
+
+export const LuopColors = {
+  Yellow: "#F2D404",
+  Red: "#FD876A",
+  Blue: "#6AAEFD",
+  Neutro: {
+    Black: "#1E2329",
+    White: "#FFFFFF",
+  },
+};
